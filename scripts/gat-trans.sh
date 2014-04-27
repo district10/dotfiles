6431fb70c148d552173dca0504942465c1e08d21
Gnat TANG <gnat_tang@yeah.net>
2014-04-27 19:21:32 +0800

    added output style illustration

--- scripts/gat-trans.sh
+++ scripts/gat-trans.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+
+# Output Style: 
+#              ➜  scripts git:(master) ✗ ./gat-trans.sh example.txt
+#              北国风光，千里冰封，万里雪飘。
+#              [etmh][r][tetit][tetir]，[tkd][tmx][tesem][etag]，[etmu][tmx][tesxc][teoss]。
+#              望长城内外，惟余莽莽；
+#              [tetas][tlk][terin][tai][tco]，[tesho][tda][temsx][temsx]；
+
 paragraph=$1 # get char from command line
 
 # Change it to your dict path, I use this: https://github.com/district10/dotfiles/tree/master/scripts/data/gat-dict.txt
