22e2e38193bcc4128328f652b9e046e6ba7236b4
Gnat TANG <gnat_tang@yeah.net>
2014-04-27 19:17:21 +0800

    开玩笑的

--- scripts/dvpe2qwerty.sh
+++ scripts/dvpe2qwerty.sh
@@ -2,6 +2,7 @@
 
 echo qwerty sucks, and this script will not work!
 echo "I want to, but its really terrible"
+echo "I know that qwerty users dont like it"
 echo "========================================================"
 echo "              #####    #   #  ######"
 echo "              #    #    # #   #"
