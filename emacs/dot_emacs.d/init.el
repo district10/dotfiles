105949f72c237e8761009f8dbc842e5d0db78e65
Gnat TANG <gnat_tang@yeah.net>
2014-05-07 20:17:17 +0800

    modified init.el, make lexical binding

--- emacs/dot_emacs.d/init.el
+++ emacs/dot_emacs.d/init.el
@@ -1,3 +1,4 @@
+;; -*- lexical-binding: t -*-
 ;;; init.el --- 
 ;; 
 ;; Filename: init.el
@@ -176,6 +177,23 @@
 ;; Evil-Mode, Fantastic~
 (require 'evil)
 (evil-mode 1)
+;; change mode-line color by evil state
+(lexical-let ((default-color (cons (face-background 'mode-line)
+                                   (face-foreground 'mode-line))))
+  (add-hook 'post-command-hook
+            (lambda ()
+              (let ((color (cond ((minibufferp) default-color)
+                                 ((evil-insert-state-p) '("#e80000" . "#ffffff"))
+                                 ((evil-emacs-state-p)  '("#444488" . "#ffffff"))
+                                 ((buffer-modified-p)   '("#006fa0" . "#ffffff"))
+                                 (t default-color))))
+                (set-face-background 'mode-line (car color))
+                (set-face-foreground 'mode-line (cdr color))))))
+
+
+
+
+
 ;;(setq evil-default-state 'emacs) 
 ;;(define-key evil-emacs-state-map (kbd "C-t") 'evil-execute-in-normal-state) 
 
